a7d0082b09bd050e7e74f824913a82b42a7fe8fa
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-05-07 16:50:25 -0400

    Create 07-associations.rb

---
+++ 07-associations.rb
@@ -0,0 +1,5 @@
+Which one of the following is not a many-to-many association? book and chapter
+In Rails 5.0, an object that had a belongs to relationship could be saved to the database even if it did not have a parent object associated with it.
+_____ is a one-to-many association. Department and employees
+_____ is an invalid association. Many-to-none
+many-to-many association: project and collaborators, student and course, blog post and categories, Order and order detail, Employee and computer, Student and course
